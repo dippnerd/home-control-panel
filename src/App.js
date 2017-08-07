c145e98b35d49a976bdf7b40e97c285e557a7bd9
Aaron Dippner <aaron@apollo.phl>
2017-08-07 16:30:57 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import Rooms from './Rooms';
 import Music from './Music';
 import Video from './Video';
 
+// const componentMap = {
+//     weather: Weather,
+//     lights: Lights
+// };
+
 const componentMenu = {
     weather: { label: 'Weather', component: Weather, icon: 'raindrop' },
     lights: { label: 'Lights', component: Lights, icon: 'lightbulb' },
@@ -15,20 +20,73 @@ const componentMenu = {
     video: { label: 'Video', component: Video, icon: 'film' }
 };
 
-class MenuButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-    }
+// const menuItems = componentMenu.map((number) =>
+//   <li>{number.label}</li>
+// );
 
-    handleClick(e) {
-        this.props.onMenuButtonClick(e.target.componentName);
-    }
+// class Button extends React.Component {
+//   render() {
+//     //  for (var x in componentMenu){
+//     //     if (!componentMenu.hasOwnProperty(x)) {
+//     //        continue;
+//     //     }
+//     //     alert(x);
+//     //     alert(componentMenu[x].label);
+//     //  }
+//         //alert(this.props.name);
+//     return (<div><button onClick = {() => this.setState({ component: this.props.name })}>
+//             {this.props.label}
+//             </button></div>);
+//   }
+// }
+
+class ButtonList extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     //this.handleChange = this.handleChange.bind(this);
+    // }
+
+    // handleChange(e) {
+    //     this.props.onTemperatureChange(e.target.value);
+    // }
 
     render() {
-        return <button onClick={this.props.handleClick}>{this.props.name}</button>;
+        // function Button(props){
+        //     return (<div><button onClick={this.props.onClick}>{props.label}</button></div>);
+        // }
+
+        var buttons = [];
+        for (var button in this.props.buttons){
+            // if (!this.props.buttons.hasOwnProperty(button)) {
+            //     continue;
+            // }
+            buttons.push(<div><button onClick={this.props.onSomeEvent}>{this.props.buttons[button].label}</button></div>);
+            //buttons.push(<Button label={this.props.buttons[button].label} name={button} />);
+        }
+        return <div>{buttons}</div>;
     }
 }
+
+// class MenuButton extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.handleClick = this.handleClick.bind(this);
+//     }
+
+//     handleClick(e) {
+//         this.props.onMenuButtonClick(e.target.componentName);
+//     }
+
+//     render() {
+//         return <button onClick = { this.props.handleClick } > { this.props.name } </button>;
+//     }
+// }
+
+// const menuItems = componentMenu.map((item) => 
+//     <button onClick = {() => this.setState({ component: item.component }) }>
+//         {item.label}
+//     </button> 
+// );
 
 class App extends Component {
     constructor(props) {
@@ -42,25 +100,33 @@ class App extends Component {
     }
 
     changeChild(componentName) {
-        this.setState({component: componentName});
+        this.setState({ component: componentName });
     }
 
     render() {
         const { component } = this.state;
-    
+
         const Child = componentMenu[component].component;
 
 
 
         return ( 
             <div>
-                <Child />
-                <MenuButton onMenuButtonClick={this.changeChild} name="Weather" componentName="weather" />
-                <MenuButton onMenuButtonClick={this.changeChild} name="Lights" componentName="lights" />
+            <Child /> 
+
+            <ButtonList onSomeEvent={this.handleClick.bind(null, componentMenu[component])} buttons={componentMenu} />
+            {/* <MenuButton onMenuButtonClick = { this.changeChild } name = "Weather" componentName = "weather" />
+
+            <MenuButton onMenuButtonClick={this.changeChild} name = "Lights" componentName = "lights" /> */}
             </div>
         );
     }
 
+
+
+    handleClick = (event, name) => {
+      this.setState({component: name});
+    }
 }
 
 export default App;
